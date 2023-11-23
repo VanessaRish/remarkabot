863f669faa8a2ed0ba31f246c708c3da317958ff
Vanessa Rish <shelterly@gmail.com>
2023-11-23 18:25:46 +0100

    initial reaction to the message

--- handlers.py
+++ handlers.py
@@ -9,3 +9,8 @@ router = Router()
 @router.message(Command("start"))
 async def start_handler(msg: Message):
     await msg.answer(f"Привет {msg.from_user.full_name}!")
+
+
+@router.message()
+async def message_handler(msg: Message):
+    await msg.answer(f"{msg.from_user.first_name}, ты сегодня молодец!")
