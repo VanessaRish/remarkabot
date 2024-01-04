1d7d409606548b4e9a6cce9c1a09ac864cd1a38c
Vanessa Rish <shelterly@gmail.com>
2024-01-04 12:53:20 +0100

    Get user chat id

--- handlers.py
+++ handlers.py
@@ -4,6 +4,7 @@ from aiogram.filters import Command
 
 import random
 from phrases import PHRASES
+from db_setup import users_collection
 
 
 router = Router()
@@ -11,8 +12,23 @@ router = Router()
 
 @router.message(Command("start"))
 async def start_handler(msg: Message):
-    print(msg.chat.id)
-    await msg.answer(f"Привет {msg.from_user.full_name}!")
+    user_info = {
+        "user_id": msg.from_user.id,
+        "username": msg.from_user.full_name,
+        "chat_id": msg.chat.id,
+        "status": "active",
+    }
+    existing_user = users_collection.find_one({"user_id": user_info["user_id"]})
+
+    if existing_user:
+        users_collection.update_one(
+            {"user_id": user_info["user_id"]},
+            {"$set": {"status": "active"}}
+        )
+        await msg.reply("Я рада, что ты вернулась!")
+    else:
+        users_collection.insert_one(user_info)
+        await msg.answer(f"Привет {msg.from_user.full_name}!")
 
 
 @router.message()
